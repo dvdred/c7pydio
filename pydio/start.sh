7d75e5f553d8251dcb3a16159546150929c4b78b
root <root@srcmedia.src.cnr.it>
2016-07-23 14:37:14 +0200

    Fix permission folders

--- pydio/start.sh
+++ pydio/start.sh
@@ -5,6 +5,7 @@
 # in case it doesn't exist
 yum update -y
 bash /bin/mkcert.sh
+chmod -R a+rw /var/lib/pydio/plugins/boot.conf /var/cache/pydio /var/lib/pydio/files /var/lib/pydio/personal /var/lib/pydio/public
 
 # Make sure we're not confused by old, incompletely-shutdown httpd
 # context after restarting the container.  httpd won't start correctly
